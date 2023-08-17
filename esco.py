9d20aef96d18a39b10e239ce9568748f70749fa4
hernanifran <129769210+hernanifran@users.noreply.github.com>
2023-08-17 19:10:41 -0300

    Update esco.py
    Mejora en los comentarios y variables

--- esco.py
+++ esco.py
@@ -10,16 +10,11 @@ nltk.download('stopwords')
 nltk.download('punkt')
 
 # Leer el archivo CSV
-df = pd.read_csv('C:/Users/Hernán Ifrán/Downloads/RECLAMOS1.csv',encoding='latin-1') # Cambia la ruta al archivo CSV
+df = pd.read_csv('C:/Users/Hernán Ifrán/Downloads/RECLAMOS1.csv',encoding='latin-1') 
 df.dropna(subset=['problema_id', 'obsitem'], inplace=True)
-# Obtener las columnas de diagnósticos y códigos AIS
+# Obteniene las columnas donde se concatena varios campos junto con la observacion y el identificador unico del problema
 Oitems = df['Concaobsitem'].tolist()
 ids = df['problema_id'].tolist()
-
-# Función para separar diagnósticos en una descripción
-def separar_diagnosticos(descripcion):
-    pattern = r'\s*\+\s*|\s+(?<!\w)\.\s+|\n'
-    return re.split(pattern, descripcion.strip())
 
 # Función para realizar stemming en una descripción
 stemmer = SnowballStemmer('spanish')
@@ -28,45 +23,45 @@ def stem_descripcion(descripcion):
     words = [stemmer.stem(word) for word in words if word not in stopwords.words('spanish')]
     return ' '.join(words)
 
-# Aplicar stemming a las descripciones
+# Aplicar stemming al campo Oitems donde se tiene la observación del reclamo
 descripciones_stemmed = [stem_descripcion(desc) for desc in Oitems]
 
 # Leer el problemas nuevo desde el archivo CSV
-archivo_diagnosticos_csv = 'C:/Users/Hernán Ifrán/Downloads/testesco.xlsx'  # Cambia la ruta al archivo CSV
-df_diagnosticos = pd.read_excel(archivo_diagnosticos_csv)
+Reclamo_csv = 'C:/Users/Hernán Ifrán/Downloads/testesco.xlsx'  
+df_Reclamo = pd.read_excel(Reclamo_csv)
 
-# Obtener la columna de diagnósticos del DataFrame
-columna_problemas = 'obsitem'  # Cambia el nombre de la columna de diagnósticos
-problemas_nuevos = df_diagnosticos[columna_problemas].tolist()
+# Obtener la columna de observacion item  del DataFrame
+columna_problemas = 'obsitem'  
+problemas_nuevos = df_Reclamo[columna_problemas].tolist()
 
 # Crear un vectorizador TF-IDF
 vectorizer = CountVectorizer()
 X = vectorizer.fit_transform(descripciones_stemmed)
 
 # Crear una lista para almacenar los resultados
-resultados1 = []
+resultados = []
 
-# Calcular la similitud coseno y los códigos AIS más similares para cada diagnóstico nuevo
+# Calcular la similitud coseno y los problemas_id más similares para cada nuevo reclamo
 for problema_nuevo in problemas_nuevos:
-    # Aplicar stemming al diagnóstico nuevo
+    # Aplicar stemming al obsitem nuevo
     problema_nuevo_stemmed = stem_descripcion(problema_nuevo.strip())
 
-    # Transformar la descripción del diagnóstico nuevo
+    # Transformar la descripción de las nuevas observaciones
     problema_nuevo_vector = vectorizer.transform([problema_nuevo_stemmed])
 
-    # Calcular la similitud coseno entre el diagnóstico nuevo y las descripciones existentes
+    # Calcular la similitud coseno con nuevos casos y las observaciones existentes
     similarities = cosine_similarity(X, problema_nuevo_vector)
 
     # Obtener el índice de la descripción más similar
     most_similar_index = np.argmax(similarities)
 
-    # Obtener el código AIS correspondiente a la descripción más similar
+    # Obtener el problema_id correspondiente a la observación más similar
     most_similar_codigo_AIS = ids[most_similar_index]
 
-    resultados1.append((most_similar_codigo_AIS, problema_nuevo.strip()))
+    resultados.append((most_similar_codigo_AIS, problema_nuevo.strip()))
 
 # Crear un DataFrame a partir de los resultados
-resultado_df = pd.DataFrame(resultados1, columns=['problema_id', 'problema_n'])
+resultado_df = pd.DataFrame(resultados, columns=['problema_id', 'problema_n'])
 
 # Guardar el DataFrame en un archivo CSV
 resultado_csv = ('C:/Users/Hernán Ifrán/Downloads/testresultado.csv')
